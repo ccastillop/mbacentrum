804485b9f0429876defd9495b50925add47877cd
Cristiam Castillo <ccastillop@gmail.com>
2010-12-20 17:45:38 -0500

    invitable spanish

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
         :recoverable, :rememberable, :trackable, :validatable, 
-        :devise_oauth2_facebook, :validate_on_invite => true
+        :devise_oauth2_facebook#, :validate_on_invite => true
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
